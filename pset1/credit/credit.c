1860169122f721e5a8146f11653610c0ec749969
foodProcessor <michael.habinsky@gmail.com>
2021-05-14 05:43:32 +0000

    Rip out all debug prints.

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -4,7 +4,6 @@
 
 int main(void)
 {
-    bool debug = true;
     bool validNumber = false;
     // Prompt for a credit card number
     long int creditCardNumber;
@@ -16,19 +15,14 @@ int main(void)
         // check for which card type this is
         // AmEx (15 digits starting with 34 or 37)
         bool isAmEx34 = creditCardNumber >= 34e13 && creditCardNumber < 35e13;
-        if (debug) { printf("isAmEx34: %i\n", isAmEx34); }
         bool isAmEx37 = creditCardNumber >= 37e13 && creditCardNumber < 38e13;
-        if (debug) { printf("isAmEx37: %i\n", isAmEx37); }
         bool isAmEx = isAmEx34 || isAmEx37;
         // Visa (13 or 16 digits starting with 4)
         bool isVisa13 = creditCardNumber >= 4e12 && creditCardNumber < 5e12;
-        if (debug) { printf("isVisa13: %i\n", isVisa13); }
         bool isVisa16 = creditCardNumber >= 4e15 && creditCardNumber < 5e15;
-        if (debug) { printf("isVisa16: %i\n", isVisa16); }
         bool isVisa = isVisa13 || isVisa16;
         // MasterCard (16 digits starting with 51-55)
         bool isMasterCard = creditCardNumber >= 51e14 && creditCardNumber < 56e14;
-        if (debug) { printf("isMasterCard: %i\n", isMasterCard); }
 
         // if the basic pattern doesn't match, start over
         if (!(isAmEx || isVisa || isMasterCard))
@@ -49,9 +43,7 @@ int main(void)
         while (remainingNumber > 0)
         {
             // get the last two digits
-            if (debug) { printf("Remaining number: %li\n", remainingNumber); }
             int thisChunk = remainingNumber % 100;
-            if (debug) { printf("Chunk: %i\n", thisChunk); }
             // remove them from the remaining number
             remainingNumber /= 100;
 
@@ -64,13 +56,11 @@ int main(void)
             // add the digits of tens*2
             evenx2DigitSum += tensx2 / 10;
             evenx2DigitSum += tensx2 % 10;
-            if (debug) { printf("evenx2DigitSum + %i + %i = %i", tensx2 / 10, tensx2 % 10, evenx2DigitSum); }
             // add the ones to the odds
             oddSum += ones;
         }
         // add the sums together
         int checksum = evenx2DigitSum + oddSum;
-        if (debug) { printf("evenx2DigitSum + oddSum = %i + %i = %i\n", evenx2DigitSum, oddSum, checksum); }
 
         // check if it's 0 mod 10
         validNumber = checksum % 10 == 0;
