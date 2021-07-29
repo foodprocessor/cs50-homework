e2a9f526af5efa311f916ab7dc23b214c09df01d
foodProcessor <michael.habinsky@gmail.com>
2021-07-29 09:33:02 +0000

    Remove memory debug.

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -5,14 +5,11 @@
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     // examine the pointers created by calloc
-    printf("after calloc, image is set to %p\n", image;
     for (int row = 0; row < height; row++)
     {
-        printf("&image[%i] is %p, and image[%i] is %p\n", row, &image[row], row, image[row]);
         for (int col = 0; col < width; col++)
         {
             RGBTRIPLE *pixel = &image[row][col];
-            printf("&image[%i][%i] is %p, and image[%i][%i] is #%x%x%x\n", row, col, &image[row][col], row, col, pixel->rgbtRed, pixel->rgbtGreen, pixel->rgbtBlue);
         }
     }
     return;
