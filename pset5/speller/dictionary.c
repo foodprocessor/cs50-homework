46b8750af82e9d3952f059d549edf74f02e431f8
foodProcessor <michael.habinsky@gmail.com>
2021-08-27 18:24:31 +0000

    Working single bucket implementation (no hash function yet).

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -1,7 +1,10 @@
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -21,7 +24,30 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
+    // hash the word
+    unsigned int wordHash = hash(word);
+    // walk the linked list for this hash
+    for (node *wordNode = table[wordHash]; wordNode != NULL; wordNode = wordNode->next)
+    {
+        int i = 0;
+        while(true)
+        {
+            if (tolower(word[i]) != tolower(wordNode->word[i]))
+            {
+                // not a match
+                // break to look at the next entry in the linked list
+                break;
+            }
+            else if (word[i] == '\0' && wordNode->word[i] == '\0')
+            {
+                // both words have ended, without finding a difference
+                return true;
+            }
+            // look at the next letter
+            i++;
+        }
+    }
+    // no match
     return false;
 }
 
@@ -35,20 +61,74 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // opent the file
+    FILE *fileHandle = fopen(dictionary, "r");
+    if (fileHandle == NULL)
+    {
+        printf("Failed to open file: %s\n", dictionary);
+        return false;
+    }
+    // iterate over incoming dictionary words
+    int numWords = 0;
+    int matches;
+    do
+    {
+        // create a new node
+        node *newWordNode = malloc(sizeof(node));
+        newWordNode->next = NULL;
+        // Brian says to use fscanf. Good idea!
+        matches = fscanf(fileHandle, "%s\n", newWordNode->word);
+        if (matches <= 0)
+        {
+            // no words were read in
+            printf("Reached end of dictionary. Total words: %d\n", numWords);
+            free(newWordNode);
+            break;
+        }
+        // now let's insert the word into the hash table
+        // first, hash the word using the has function
+        unsigned int wordHash = hash(newWordNode->word);
+        // to insert into the linked list, first copy the head pointer to the new node's next field
+        newWordNode->next = table[wordHash];
+        // now replace the head with the new node
+        table[wordHash] = newWordNode;
+    } while(!feof(fileHandle));
+    // tell the caller of our success
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    int numWords = 0;
+    for (int bucket = 0; bucket < N; bucket++)
+    {
+        for (node *wordNode = table[bucket]; wordNode != NULL; wordNode = wordNode->next)
+        {
+            numWords++;
+        }
+    }
+    return numWords;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    // node *temp, *nodeToFree;
+    for (int bucket = 0; bucket < N; bucket++)
+    {
+        // free from the head
+        node *nodeToFree = table[bucket];
+        // don't free NULL pointers!
+        while (nodeToFree != NULL)
+        {
+            // move the list head forward
+            table[bucket] = table[bucket]->next;
+            // free the old head of the list
+            free(nodeToFree);
+            // get ready for the next iteration
+            nodeToFree = table[bucket];
+        }
+    }
+    return true;
 }
